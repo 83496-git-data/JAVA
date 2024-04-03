93e05d8e6b9376d5c07834ef06dd0093195086ee
kd4-sabir-83496 <sabirmujawar1999@gmail.com>
2024-04-03 16:59:23 +0530

    Assignment02-1 uploaded

---
+++ JAVA-LAB/Assignment02-1/src/com/sunbeam/tester/InvoiceTester.java
@@ -0,0 +1,18 @@
+package com.sunbeam.tester;
+
+import com.sunbeam.invoice.Invoice;
+
+public class InvoiceTester {
+
+	public static void main(String[] args) {
+		
+		Invoice i = new Invoice("1001","Mouse",2,2000);
+		System.out.println("PART NUMBEAR = "+i.getPnum());
+		System.out.println("PART DESCRIPTION = "+i.getPdesc());
+		System.out.println("PART QUNTITY = "+i.getQty());
+		System.out.println("PART PRICE = "+i.getPrice());
+		System.out.println("CALCULATE INVOICE = "+i.calculateInvoice());
+		
+	}
+
+}
